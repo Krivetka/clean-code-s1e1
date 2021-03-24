e4a3c3729cadf309a5f16fbdc13ceac8b4359aec
IVAN KULIASHOV <dfyreybr@gmail.com>
2021-03-24 21:41:59 +0300

    fix: Use BEM according to rule 2.1(advanced level)

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed");//completed-tasks
+var incompleteTaskHolder=document.getElementsByClassName("incomplete-Tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementsByClassName("completed");//completed-tasks
 
 
 //New task list item
